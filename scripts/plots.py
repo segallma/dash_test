304a8f1eef4e4be6519cdc27780e7c4eec7b3479
SegallMA <marcelosegall@gmail.com>
2026-02-20 19:15:57 -0300

    fix inverted axes labels

--- scripts/plots.py
+++ scripts/plots.py
@@ -14,29 +14,29 @@ def lineplot(df, x_col, y_col, color_col = None , line_col = None):
     # Set layout and styling compactly
     
     p.update_layout(
-    template="simple_white",         # theme_bw equivalent
-    title_text="",
-    title_font=dict(color="black", size=18, family="Arial, sans-serif"))
+    template = "simple_white",         # theme_bw equivalent
+    title_text = "",
+    title_font = dict(color = "black", size = 18, family = "Arial, sans-serif"))
 
     # Axis styling
-    p.update_xaxes(title_text="Rate", title_font_size=20, title_font_color="black",
-                 tickfont_size=14, tickfont_color="black")
+    p.update_xaxes(
+        title_text = "Years", 
+        title_font_size = 20,
+        title_font_color = "black",
+        tickfont_size = 14, 
+        tickfont_color = "black",
+        showgrid = True,
+        gridcolor = "lightgray",
+        gridwidth = 1)
     
-    p.update_yaxes(title_text="Years", title_font_size=20, title_font_color="black",
-                 tickfont_size=14, tickfont_color="black")
+    p.update_yaxes(
+        title_text = "Rate",
+        title_font_size = 20, 
+        title_font_color = "black",
+        tickfont_size = 14,
+        tickfont_color = "black")
     
     return p
 
 
 
-
-
-#plot = lineplot(
-#    df_plot,
-#    x_col = "year",
-#    y_col = "rate",
-#    color_col = "group", 
-#    line_col = "sex")
-
-
-#plot.show(renderer = "browser")
