2dd5402953c5d062e35ebe07802f1c054eb49c3c
SegallMA <marcelosegall@gmail.com>
2026-02-21 17:38:58 -0300

    tinkering with data paths so it works with render

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -7,11 +7,6 @@ import pandas as pd
 import os
 
 def load_cases_rates(filename="cases_by_group.csv"):
-
-    base_dir = os.path.dirname(__file__)
-    
-    path = os.path.join(base_dir, "..", "data", filename)
-    
-    df0 = pd.read_csv(path, delimiter=";")
-    
-    return df0
+    # path relative to app root (WORKDIR)
+    path = os.path.join("data", filename)
+    return pd.read_csv(path, delimiter=";")
