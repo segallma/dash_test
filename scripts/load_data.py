35d2c0b6ca91fc00b08038bdbb2ce2ac435c0c85
SegallMA <marcelosegall@gmail.com>
2026-02-21 15:35:41 -0300

    importing paths now work across distinct OS

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,9 +1,17 @@
+# Import data
+
+# Building the path instead of giving it as a string to solve backslash-related 
+# incompatibilities between systems
+
 import pandas as pd
+import os
 
-def load_cases_rates(path = ".\data\cases_by_group.csv"):
-    df0 = pd.read_csv(
-    path,
-    delimiter = ";")
+def load_cases_rates(filename="cases_by_group.csv"):
+
+    base_dir = os.path.dirname(__file__)
+    
+    path = os.path.join(base_dir, "..", "data", filename)
+    
+    df0 = pd.read_csv(path, delimiter=";")
+    
     return df0
-
-
